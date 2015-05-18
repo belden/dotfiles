e8ce2391bcaccdf4e4260bf5dfe5e64cf9d20f9e
Belden Lyman <belden.lyman@gmail.com>
2015-05-18 13:39:36 -0400

    emacs: cperl-mode: <f5> - compile with 'perl'

--- emacs24/elisp/belden/hotkeys-mode.el
+++ emacs24/elisp/belden/hotkeys-mode.el
@@ -4,7 +4,7 @@
   :global t
   :keymap (let ((map (make-sparse-keymap)))
 	    (define-key map (kbd "<f4>") 'belden-follow)
-	    (define-key map (kbd "<f5>") 'belden-test-this)
+	    (define-key map (kbd "<f5>") 'compile)  ;; see below for treatment of 'compile-command in 'cperl-mode-hook
 	    (define-key map (kbd "<f6>") 'belden-next-error-recenter)
 	    (define-key map (kbd "<f7>") 'scott-window-mdi-maximize-restore-toggle)
 	    (define-key map (kbd "<f8>") 'belden-hide-this-buffer)
@@ -14,6 +14,13 @@
 	    (define-key map (kbd "<f11>") 'other-window)
 	    (define-key map (kbd "<f12>") 'font-lock-mode)
 	    map))
+
+;; for <f5>
+(add-hook 'cperl-mode-hook
+	  (lambda ()
+	    (set (make-local-variable 'compile-command)
+		 (concat "perl " (buffer-file-name)))))
+
 
 (load "scott-window.el") ;; for <f7>
 
