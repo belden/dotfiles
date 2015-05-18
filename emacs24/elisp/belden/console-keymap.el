0ffc679af88866928a882cb860cf315a93ed957f
Belden Lyman <belden.lyman@gmail.com>
2015-05-18 17:21:10 -0400

    emacs24: don't refer to terminal-specific keycodes
    Instead of this:
    
        (define-key map (kbd "\e[19;5~") 'nav-toggle)            ;; C-<f8>
    
    We can write:
    
        (define-key input-decode-map "\e[19;5~" [C-f8])
        (define-key map (kbd "<C-f8>") 'nav-toggle)
    
    This has the advantage of requiring more keystrokes to achieve the
    desired configuration, so it's probably more correct.

---
+++ emacs24/elisp/belden/console-keymap.el
@@ -0,0 +1,27 @@
+;; (define-key input-decode-map "\e[1;2A]" [S-up])
+;; (define-key input-decode-map "\e[1;3A]" [M-up])
+;; (define-key input-decode-map "\e[1;4A]" [S-M-up])
+;; (define-key input-decode-map "\e[1;5A]" [C-up])
+;; (define-key input-decode-map "\e[1;6A]" [S-C-up])
+
+;; (define-key input-decode-map "\e[1;2B]" [S-down])
+;; (define-key input-decode-map "\e[1;3B]" [M-down])
+;; (define-key input-decode-map "\e[1;4B]" [S-M-down])
+;; (define-key input-decode-map "\e[1;5B]" [C-down])
+;; (define-key input-decode-map "\e[1;6B]" [S-C-down])
+
+;; (define-key input-decode-map "\e[1;2C]" [S-right])
+;; (define-key input-decode-map "\e[1;3C]" [M-right])
+;; (define-key input-decode-map "\e[1;4C]" [S-M-right])
+;; (define-key input-decode-map "\e[1;5C]" [C-right])
+;; (define-key input-decode-map "\e[1;6C]" [S-C-right])
+
+;; (define-key input-decode-map "\e[1;2D]" [S-left])
+;; (define-key input-decode-map "\e[1;3D]" [M-left])
+;; (define-key input-decode-map "\e[1;4D]" [S-M-left])
+;; (define-key input-decode-map "\e[1;5D]" [C-left])
+;; (define-key input-decode-map "\e[1;6D]" [S-C-left])
+
+
+(define-key input-decode-map "\e[17;2~" [S-f6])
+(define-key input-decode-map "\e[19;5~" [C-f8])
--- emacs24/elisp/belden/hotkeys-mode.el
+++ emacs24/elisp/belden/hotkeys-mode.el
@@ -5,26 +5,33 @@
   :keymap (let ((map (make-sparse-keymap)))
 	    (define-key map (kbd "<f4>") 'belden-follow)
 	    (define-key map (kbd "<f5>") 'compile)  ;; see below for treatment of 'compile-command in 'cperl-mode-hook
-	    (define-key map (kbd "<f6>") 'belden-next-error-recenter)
+	    (define-key map (kbd "<f6>") 'belden/next-error-recenter)
+	    (define-key map (kbd "<S-f6>") 'belden/previous-error-recenter)
 	    (define-key map (kbd "<f7>") 'scott-window-mdi-maximize-restore-toggle)
 	    (define-key map (kbd "<f8>") 'belden-hide-this-buffer)
-	    (define-key map (kbd "\e[19;5~") 'nav-toggle)            ;; C-<f8>
+	    (define-key map (kbd "<C-f8>") 'nav-toggle)
 	    (define-key map (kbd "<f9>") 'belden-perl-debug)
-	    (define-key map (kbd "<f10>") 'belden-comment-dwim)
+	    (define-key map (kbd "<f10>") 'comment-dwim)
 	    (define-key map (kbd "<f11>") 'other-window)
 	    (define-key map (kbd "<f12>") 'font-lock-mode)
 	    map))
 
 ;; for <f5>
-(add-hook 'cperl-mode-hook
-	  (lambda ()
-	    (set (make-local-variable 'compile-command)
-		 (concat "perl " (buffer-file-name)))))
+(defun belden/hotkeys-mode/cperl-compilation ()
+  (set (make-local-variable 'compile-command)
+       (concat "perl " (buffer-file-name))))
+(add-hook 'cperl-mode-hook 'belden/hotkeys-mode/cperl-compilation)
 
+;; for <f7>
+(load "scott-window.el")
 
-(load "scott-window.el") ;; for <f7>
-
+;; for <f8>
 (setq nav-disable-overager-window-splitting t)
-(require 'nav) ;; for <f8>
+(require 'nav)
+
+;; for <f10>
+(defun belden-comment-dwim (arg)
+  (interactive "*P")
+  (comment-dwim arg))
 
 (provide 'belden/hotkeys-mode)
