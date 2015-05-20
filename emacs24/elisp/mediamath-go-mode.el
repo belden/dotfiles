a17655bbe7696a5b681ec74b274d7e9a4b1a0d5a
Belden Lyman <belden.lyman@gmail.com>
2015-05-20 13:25:52 -0400

    mm-go-mode: C-x r f: run this file

--- emacs24/elisp/mediamath-go-mode.el
+++ emacs24/elisp/mediamath-go-mode.el
@@ -2,13 +2,21 @@
 
 (require 'go-mode)
 
-(defun run-selected-golang-code ()
+(defun go-run-region-locally ()
   "run the selected code as though it is golang code"
   (interactive)
   (let ((tempfile (format "%s.go" (make-temp-file "golang"))))
     (write-region (region-beginning) (region-end) tempfile)
     (compile (format "go run %s" tempfile))))
 
+(defun go-run-file-locally ()
+  "run the current file as though it is golang code"
+  (interactive)
+  (save-some-buffers)
+  (compile (format "go run %s" (buffer-file-name))))
+
 (define-derived-mode mediamath-go-mode go-mode
   "MM Go"
-  (define-key mediamath-go-mode-map (kbd "C-x r r") 'run-selected-golang-code))
+  (define-key mediamath-go-mode-map (kbd "C-x r r") 'go-run-region-locally)
+  (define-key mediamath-go-mode-map (kbd "C-x r f") 'go-run-file-locally)
+  )
