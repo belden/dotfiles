a42d321b72ad6eba9b5d6f9b8cebcd1b36448c99
Belden Lyman <belden@retailnext.net>
2016-04-15 11:45:11 -0700

    frontend file detection
    .less -> less-css-mode
    .hbs  -> handlebars-mode

--- emacs24/elisp/belden/filetypes.el
+++ emacs24/elisp/belden/filetypes.el
@@ -1,5 +1,7 @@
 (autoload 'belden-cperl-mode "belden-cperl-mode" nil t)
 (add-to-list 'auto-mode-alist '("\\.\\([pP][Llm]\\|[tT]\\)\\'" . belden-cperl-mode))
+(add-to-list 'auto-mode-alist '("\\.hbs'" . handlebars-mode))
+(add-to-list 'auto-mode-alist '("\\.less'" . less-css-mode))
 (add-to-list 'interpreter-mode-alist '("perl" . belden-cperl-mode))
 (add-to-list 'interpreter-mode-alist '("perl5" . belden-cperl-mode))
 (add-to-list 'interpreter-mode-alist '("miniperl" . belden-cperl-mode))
